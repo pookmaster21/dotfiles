2eed773dc2224433cbc744d694f6fc6f35509d6a
pookmaster21 <landa.lavi@gmail.com>
2025-04-21 14:14:51 +0300

    added bashls to neovim

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,125 +1,130 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-				ensure_installed = {
-					"rust_analyzer",
-					"black",
-					"isort",
-					"clang-format",
-					"codelldb",
-					"debugpy",
-					"delve",
-					"gofumpt",
-					"goimports-reviser",
-					"golines",
-					"gomodifytags",
-					"mypy",
-					"prettier",
-					"rubocop",
-					"stylua",
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"cssls",
-					"dockerls",
-					"eslint",
-					"hls",
-					"jsonls",
-					"lua_ls",
-					"ruby_lsp",
-					"tailwindcss",
-					"ts_ls",
-					"zls",
-					"jdtls",
-					"hyprls",
-					"pyright",
-					"gopls",
-					"clangd",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+        ensure_installed = {
+          "rust_analyzer",
+          "black",
+          "isort",
+          "clang-format",
+          "codelldb",
+          "debugpy",
+          "delve",
+          "gofumpt",
+          "goimports-reviser",
+          "golines",
+          "gomodifytags",
+          "mypy",
+          "prettier",
+          "rubocop",
+          "stylua",
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "cssls",
+          "dockerls",
+          "eslint",
+          "hls",
+          "jsonls",
+          "lua_ls",
+          "ruby_lsp",
+          "tailwindcss",
+          "ts_ls",
+          "zls",
+          "jdtls",
+          "hyprls",
+          "pyright",
+          "gopls",
+          "clangd",
+          "bashls",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.dockerls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.dockerls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.eslint.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.eslint.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.hls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.hls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.jsonls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.ruby_lsp.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.ruby_lsp.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.zls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.zls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.jdtls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.jdtls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.hyprls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.hyprls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
-		end,
-	},
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.bashls.setup ({
+        capabilities = capabilities,
+      })
+    end,
+  },
 }
